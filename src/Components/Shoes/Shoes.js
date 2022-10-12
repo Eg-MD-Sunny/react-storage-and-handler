695a53040c372768333f8a1b755a026f88b351fb
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-12 22:53:01 +0600

    Modify

--- src/Components/Shoes/Shoes.js
+++ src/Components/Shoes/Shoes.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { multiply } from '../utilities/utilities';
+import { add, multiply } from '../utilities/utilities';
 
 
 const Shoes = () => {
     const first = 13;
     const second = 11;
     const result = multiply(first,second)
+    const sum = add(first,second);
     return (
         <div>
             <h3>This is shoes Component</h3>
-            <p>Result: {result}</p>    
+            <p>Result: {result} and total: {sum}</p>    
         </div>
     );
 };
