a49e05fdf031cdfe250c5767a3903245c00a1067
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-15 00:20:46 +0600

    Modify

--- src/Components/Cosmetic/Cosmetic.js
+++ src/Components/Cosmetic/Cosmetic.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Showdata from '../Showdata/Showdata';
 
 const Cosmetic = () => {
@@ -10,38 +10,47 @@ const Cosmetic = () => {
     //     {id:5, name: 'Nalis', price:500},
     // ]
 
-    const cosmetics = [
-            {
-              "_id": "63499514943f11f6a0155c9a",
-              "price": 288,
-              "name": "Aline Hernandez"
-            },
-            {
-              "_id": "63499514e52e011cc294cad1",
-              "price": 242,
-              "name": "Dyer Olsen"
-            },
-            {
-              "_id": "634995145c6252833fc5a151",
-              "price": 51,
-              "name": "Hayden Rosales"
-            },
-            {
-              "_id": "63499514dbf2f8ea268886c9",
-              "price": 311,
-              "name": "Silvia Woods"
-            },
-            {
-              "_id": "634995143a5ad52097bda673",
-              "price": 270,
-              "name": "Calhoun Holcomb"
-            },
-            {
-              "_id": "634995141c0098675a9b5ad9",
-              "price": 164,
-              "name": "Cochran Johnson"
-            }
-    ]
+    // const cosmetics = [
+    //         {
+    //           "_id": "63499514943f11f6a0155c9a",
+    //           "price": 288,
+    //           "name": "Aline Hernandez"
+    //         },
+    //         {
+    //           "_id": "63499514e52e011cc294cad1",
+    //           "price": 242,
+    //           "name": "Dyer Olsen"
+    //         },
+    //         {
+    //           "_id": "634995145c6252833fc5a151",
+    //           "price": 51,
+    //           "name": "Hayden Rosales"
+    //         },
+    //         {
+    //           "_id": "63499514dbf2f8ea268886c9",
+    //           "price": 311,
+    //           "name": "Silvia Woods"
+    //         },
+    //         {
+    //           "_id": "634995143a5ad52097bda673",
+    //           "price": 270,
+    //           "name": "Calhoun Holcomb"
+    //         },
+    //         {
+    //           "_id": "634995141c0098675a9b5ad9",
+    //           "price": 164,
+    //           "name": "Cochran Johnson"
+    //         }
+    // ]
+
+    
+    const [cosmetics, setCosmetics] = useState([]);
+    
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setCosmetics(data))
+    },[])
     return (
         <div>
             <h1>Welcome to my cosmetic store</h1>
