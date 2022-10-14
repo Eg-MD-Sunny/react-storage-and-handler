a347027b47d0b80a5bb452fb07ced4096fe30451
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-14 23:09:10 +0600

    Modify

--- src/Components/Showdata/Showdata.js
+++ src/Components/Showdata/Showdata.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Showdata = (props) => {
-    const {name,price,id} = props.cosmetic;
+    const {name,price,_id} = props.cosmetic;
     return (
         <div>
             <h2>Buy This: {name}</h2>
             <p>Only For: ${price}</p>
-            <p><small>It has id: {id}</small></p>
+            <p><small>It has id: {_id}</small></p>
         </div>
     );
 };
