a78d29888a5508b0512536f419a4effaa7a1e724
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-16 00:21:46 +0600

    Modify

--- src/Components/Showdata/Showdata.js
+++ src/Components/Showdata/Showdata.js
@@ -6,12 +6,15 @@ const Showdata = (props) => {
     const addToCart = (_id) =>{
         console.log('item added',_id)
     }
+    const addToCartWithParam = ()=>addToCart(_id);
+
     return (
         <div className="product">
             <h2>Buy This: {name}</h2>
             <p>Only For: ${price}</p>
             <p><small>It has id: {_id}</small></p>
-            <button onClick={addToCart(_id)}>Add to Cart</button>
+            <button onClick={addToCartWithParam}>Add to Cart</button>
+            <button onClick={()=>addToCart(_id)} style={{Margin:'15px'}}>Add to Cart: shortcut</button>
         </div>
     );
 };
