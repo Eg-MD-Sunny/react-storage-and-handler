31f64e2e86e5f32932cee51076dcd16ae1e8a942
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-18 22:45:34 +0600

    Modify

--- src/Components/utilities/fakedb.js
+++ src/Components/utilities/fakedb.js
@@ -2,11 +2,9 @@
 const addTodb = _id =>{
     const quantity = localStorage.getItem(_id);
     if(quantity){
-        console.log('Already Exists')
         const newQuantity = parseInt(quantity) + 1;
         localStorage.setItem(_id, newQuantity);
     }else{
-        console.log('New Item')
         localStorage.setItem(_id,1)
     }
 }
