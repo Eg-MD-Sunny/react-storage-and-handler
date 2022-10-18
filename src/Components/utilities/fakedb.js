c22b0db81767efaef1e4bd7b2cca55e6d4a0a694
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-18 22:59:44 +0600

    Modify

--- src/Components/utilities/fakedb.js
+++ src/Components/utilities/fakedb.js
@@ -1,12 +1,19 @@
 //Use Local storage to manage cart data
 const addTodb = _id =>{
-    const quantity = localStorage.getItem(_id);
+    let shoppingCart = {};
+    
+    // const quantity = localStorage.getItem(_id);
+    const quantity = shoppingCart[_id];
     if(quantity){
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(_id, newQuantity);
+        // const newQuantity = parseInt(quantity) + 1;
+        const newQuantity = quantity + 1;
+        shoppingCart[_id] = newQuantity;
+        // localStorage.setItem(_id, newQuantity);
     }else{
-        localStorage.setItem(_id,1)
+        shoppingCart[_id] = 1;
+        // localStorage.setItem(_id,1)
     }
+    localStorage.setItem('shopping-cart',JSON.stringify(shoppingCart))
 }
 export { addTodb };
 
